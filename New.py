14e4ae3940b8ff33c8fecbb9bfdca620d6f2980b
Siddhesh <siddhesh.nikam22@gmail.com>
2025-01-15 19:40:30 +0530

    Add Changes

--- New.py
+++ New.py
@@ -3,4 +3,10 @@ import pandas as pd
 data = pd.read_csv("heart.csv")
 
 print(data.head())
+print(data.dtypes)
+print(data.info())
+print(data.shape())
+print(data.describe())
 print(data.isnull().sum())
+print(data.isnull().sum())
+
